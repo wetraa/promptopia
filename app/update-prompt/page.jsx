f03c286ab077236827f8615096b9e2aa247336e4
wetraa <whoispopov@gmail.com>
2024-11-01 13:25:20 +0500

    remarks

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -2,55 +2,56 @@
 
 import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useSession } from "next-auth/react";
 
 import Form from "@components/Form";
 
 const UpdatePrompt = () => {
-  const { data: session } = useSession();
   const router = useRouter();
   const searchParams = useSearchParams();
   const promptId = searchParams.get("id");
-  const [submitting, setSubmitting] = useState(false);
-  const [post, setPost] = useState({
-    prompt: "",
-    tag: "",
-  });
-  useEffect(() => {
-    const fetchPost = async () => {
-      const response = await fetch(`/api/prompt/${promptId}`);
-      if (response.status != 200) router.push("/profile");
 
-      const prompt = await response.json();
+  const [post, setPost] = useState({ prompt: "", tag: "" });
+  const [submitting, setIsSubmitting] = useState(false);
+
+  useEffect(() => {
+    const getPromptDetails = async () => {
+      const response = await fetch(`/api/prompt/${promptId}`);
+      const data = await response.json();
+
       setPost({
-        prompt: prompt.prompt,
-        tag: prompt.tag,
+        prompt: data.prompt,
+        tag: data.tag,
       });
     };
-    if (promptId) fetchPost();
+
+    if (promptId) getPromptDetails();
   }, [promptId]);
 
   const updatePrompt = async (e) => {
     e.preventDefault();
+    setIsSubmitting(true);
+
+    if (!promptId) return alert("Missing PromptId!");
+
     try {
-      setSubmitting(true);
       const response = await fetch(`/api/prompt/${promptId}`, {
         method: "PATCH",
         body: JSON.stringify({
-          //   userId: session?.user.id,
           prompt: post.prompt,
           tag: post.tag,
         }),
       });
+
       if (response.ok) {
-        router.push("/profile");
+        router.push("/");
       }
     } catch (error) {
       console.log(error);
     } finally {
-      setSubmitting(false);
+      setIsSubmitting(false);
     }
   };
+
   return (
     <Form
       type="Edit"
