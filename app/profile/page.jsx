41af4594f9e27f7b5b17589f5d4b958650647113
wetraa <whoispopov@gmail.com>
2024-11-01 14:24:02 +0500

    delete prompt handler: wrap 'confirm' into setTimeout

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -27,16 +27,18 @@ const ProfilePage = () => {
     router.push(`/update-prompt?id=${post._id}`);
   };
   const handleDelete = async (post) => {
-    const hasConfirmedToDelete = confirm(
-      "Are you sure you want to delete this prompt?"
-    );
-    if (hasConfirmedToDelete) {
-      await fetch(`/api/prompt/${post._id}`, {
-        method: "DELETE",
-      });
-      const filteredPosts = posts.filter((p) => p._id !== post._id);
-      setPosts(filteredPosts);
-    }
+    setTimeout(async () => {
+      const hasConfirmedToDelete = confirm(
+        "Are you sure you want to delete this prompt?"
+      );
+      if (hasConfirmedToDelete) {
+        await fetch(`/api/prompt/${post._id}`, {
+          method: "DELETE",
+        });
+        const filteredPosts = posts.filter((p) => p._id !== post._id);
+        setPosts(filteredPosts);
+      }
+    });
   };
   const handleTagClick = (tag) => {
     setSearchTag(tag);
